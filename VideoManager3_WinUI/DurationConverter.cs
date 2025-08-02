853df56642c20e242f4383625155328935b01c14
輝舟 川東 <kawachan1005abc@gmail.com>
2025-08-02 15:33:13 +0900

    ファイルサイズ、動画長さの表示用変換に対応

---
+++ VideoManager3_WinUI/DurationConverter.cs
@@ -0,0 +1,26 @@
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace VideoManager3_WinUI
+{
+    public class DurationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double seconds)
+            {
+                var ts = TimeSpan.FromSeconds(seconds);
+                if (ts.TotalHours >= 1)
+                    return ts.ToString(@"hh\:mm\:ss");
+                else
+                    return ts.ToString(@"mm\:ss");
+            }
+            return "00:00";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

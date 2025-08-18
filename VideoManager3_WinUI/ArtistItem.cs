0ea03ec3ae037405c358bcf4c1f369ccd21c2e5e
輝舟 川東 <kawachan1005abc@gmail.com>
2025-08-18 22:44:24 +0900

    アーティストに色を追加

--- VideoManager3_WinUI/ArtistItem.cs
+++ VideoManager3_WinUI/ArtistItem.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Media;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VideoManager3_WinUI {
-    public class ArtistItem {
+    public class ArtistItem:INotifyPropertyChanged {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // アーティスト名
         public string Name { get; set; } = string.Empty;
+        
+        // アーティストが関連付けられた動画のリスト
         public List<VideoItem> VideosInArtist { get; set; } = new List<VideoItem>();
+
+        // 表示用の色
+        private Brush? _color;
+        public Brush? Color {
+            get => _color;
+            set {
+                _color = value;
+                PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( nameof( Color ) ) );
+            }
+        }
+
     }
 }
